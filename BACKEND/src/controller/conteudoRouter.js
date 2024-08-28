4603c4b6bc802347bc2aac808e06a1bd86b34807
Heitor <heitor.denech@gmail.com>
2024-08-28 13:31:42 -0300

    api_geodiverso_1

--- BACKEND/src/controller/conteudoRouter.js
+++
@@ -1,9 +0,0 @@
-// const {Router} = require('express');
-
-// const router = Router();
-
-// const { getConteudoById } = require("./conteudoController");
-
-// router.get('/get/conteudo/:id', getConteudoById);
-
-// module.exports = router;
